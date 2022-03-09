db305647a36f24fbb9edd9cecbc5597bff7143a1
C Medianu <488889+cmedianu@users.noreply.github.com>
2022-03-08 18:49:10 -0800

    Pre-delete

---
+++ Infrastructure/Services/AuthenticationServiceConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Hides.Infrastructure.Persistence.Util;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using OraEmp.Infrastructure.Identity;
+
+namespace OraEmp.Infrastructure.Services
+{
+    public static class AuthenticationServiceConfiguration
+    {
+        public static IServiceCollection AddEmpAuthenticationServices(this IServiceCollection services,
+            Action<ConfigOptions> configureOptions)
+        {
+            services.Configure<ConfigOptions>(configureOptions);
+            services.AddScoped<AuthenticationStateProvider, EmpAuthenticationStateProvider>();
+            return services;
+        }
+    }
+}
